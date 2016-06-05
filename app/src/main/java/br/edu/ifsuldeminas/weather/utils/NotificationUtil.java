e18f77887618a5311f91e1b44d4482e2afe09a0e
Emerson Carvalho <assis.emerson@gmail.com>
2016-06-04 23:59:16 -0300

    First commit

--- app/src/main/java/br/edu/ifsuldeminas/weather/utils/NotificationUtil.java
+++ app/src/main/java/br/edu/ifsuldeminas/weather/utils/NotificationUtil.java
@@ -1,7 +1,6 @@
 package br.edu.ifsuldeminas.weather.utils;
 
 import android.app.Notification;
-import android.app.NotificationManager;
 import android.app.PendingIntent;
 import android.content.Context;
 import android.content.Intent;
@@ -9,9 +8,6 @@ import android.graphics.Color;
 import android.support.v4.app.NotificationCompat;
 import android.support.v4.app.NotificationManagerCompat;
 import android.support.v4.app.TaskStackBuilder;
-import android.util.Log;
-
-import java.util.List;
 
 import br.edu.ifsuldeminas.weather.R;
 
@@ -46,129 +42,5 @@ public class NotificationUtil {
         notificationManager.notify(id, builder.build());
     }
 
-    public static void notify(Context context, int id, Intent intent, String contentTitle, String contentText) {
-        NotificationManager manager =
-                (NotificationManager) context.getSystemService(Context.NOTIFICATION_SERVICE);
-
-        // Intent para disparar o broadcast
-        PendingIntent pendingIntent = PendingIntent.getActivity(context, 0, intent, PendingIntent.FLAG_UPDATE_CURRENT);
-
-        // Cria a notification
-        NotificationCompat.Builder builder = new NotificationCompat.Builder(context)
-                .setContentIntent(pendingIntent)
-                .setContentTitle(contentTitle)
-                .setContentText(contentText)
-                .setSmallIcon(R.mipmap.ic_launcher)
-                .setAutoCancel(true);
-
-        // Dispara a notification
-        Notification notification = builder.build();
-        manager.notify(id, notification);
-
-        Log.d(TAG, "Notification criada com sucesso");
-    }
-
-    public static void createBig(Context context, Intent intent, String contentTitle, String contentText,List<String> lines, int id) {
-        PendingIntent p = getPendingIntent(context, intent, id);
-
-        // Configura o estilo Inbox
-        int size = lines.size();
-        NotificationCompat.InboxStyle inboxStyle = new NotificationCompat.InboxStyle();
-        inboxStyle.setBigContentTitle(contentTitle);
-        for (String s: lines) {
-            inboxStyle.addLine(s);
-        }
-        inboxStyle.setSummaryText(contentText);
-
-        // Cria a notificação
-        NotificationCompat.Builder b = new NotificationCompat.Builder(context);
-        b.setDefaults(Notification.DEFAULT_ALL); // Ativa configurações padrão
-        b.setSmallIcon(R.drawable.ic_launcher); // Ícone
-        b.setContentTitle(contentTitle); // Título
-        b.setContentText(contentText); // Mensagem
-        b.setContentIntent(p); // Intent que será chamada ao clicar na notificação.
-        b.setAutoCancel(true); // Auto cancela a notificação ao clicar nela
-        b.setNumber(size); // Número para aparecer na notificação
-        b.setStyle(inboxStyle); // Estilo customizado
-
-        NotificationManagerCompat nm = NotificationManagerCompat.from(context);
-        nm.notify(id, b.build());
-    }
-
-    public static void createWithAction(Context context, Intent intent, String contentTitle, String contentText, int id) {
-        PendingIntent p = getPendingIntent(context, intent, id);
-
-        // Cria a notificação
-        NotificationCompat.Builder b = new NotificationCompat.Builder(context);
-        b.setDefaults(Notification.DEFAULT_ALL); // Ativa configurações padrão
-        b.setSmallIcon(R.drawable.ic_launcher); // Ícone
-        b.setContentTitle(contentTitle); // Título
-        b.setContentText(contentText); // Mensagem
-        b.setContentIntent(p); // Intent que será chamada ao clicar na notificação.
-        b.setAutoCancel(true); // Auto cancela a notificação ao clicar nela
-
-        // Ação customizada (deixei a mesma intent para os dois)
-        PendingIntent actionIntent = PendingIntent.getBroadcast(
-                context, 0, new Intent(ACTION_VISUALIZAR), 0);
-        b.addAction(R.drawable.ic_acao_pause, "Pause", actionIntent);
-        b.addAction(R.drawable.ic_acao_play, "Play", actionIntent);
-
-        NotificationManagerCompat nm = NotificationManagerCompat.from(context);
-        nm.notify(id, b.build());
-    }
-
-    // Notification no Android 5.0 Lollipop (Cor vermelha e heads-up e tela de bloqueio)
-    public static void createHeadsUpNotification(Context context, Intent intent, String contentTitle, String contentText, int id) {
-        PendingIntent p = getPendingIntent(context, intent, id);
-
-        // Cria a notificação
-        NotificationCompat.Builder b = new NotificationCompat.Builder(context);
-        b.setDefaults(Notification.DEFAULT_ALL); // Ativa configurações padrão
-        b.setSmallIcon(R.drawable.ic_notification_icon); // Ícone
-        b.setContentTitle(contentTitle); // Título
-        b.setContentText(contentText); // Mensagem
-        b.setContentIntent(p); // Intent que será chamada ao clicar na notificação.
-        b.setAutoCancel(true); // Auto cancela a notificação ao clicar nela
-
-        // No Android 5.0
-        b.setColor(Color.RED);
-        // Heads-up notification
-        b.setFullScreenIntent(p,false);
-        // Privada se estiver na tela de bloqueio
-        b.setVisibility(NotificationCompat.VISIBILITY_PRIVATE);
-
-        NotificationManagerCompat nm = NotificationManagerCompat.from(context);
-        nm.notify(id, b.build());
-    }
-
-    // Notification no Android 5.0 Lollipop (tela de bloqueio)
-    public static void createPrivateNotification(Context context, Intent intent, String contentTitle, String contentText, int id) {
-        PendingIntent p = getPendingIntent(context, intent, id);
-
-        // Cria a notificação
-        NotificationCompat.Builder b = new NotificationCompat.Builder(context);
-        b.setDefaults(Notification.DEFAULT_ALL); // Ativa configurações padrão
-        b.setSmallIcon(R.drawable.ic_notification_icon); // Ícone
-        b.setContentTitle(contentTitle); // Título
-        b.setContentText(contentText); // Mensagem
-        b.setContentIntent(p); // Intent que será chamada ao clicar na notificação.
-        b.setAutoCancel(true); // Auto cancela a notificação ao clicar nela
-
-        // PUBLIC, PRIVATE, SECRET
-        b.setVisibility(NotificationCompat.VISIBILITY_SECRET);
-
-        NotificationManagerCompat nm = NotificationManagerCompat.from(context);
-        nm.notify(id, b.build());
-    }
-
-    public static void cancell(Context context, int id) {
-        NotificationManagerCompat nm = NotificationManagerCompat.from(context);
-        nm.cancel(id);
-    }
-
-    public static void cancellAll(Context context) {
-        NotificationManagerCompat nm = NotificationManagerCompat.from(context);
-        nm.cancelAll();
-    }
 }
 
